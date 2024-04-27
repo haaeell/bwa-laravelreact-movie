9bab7349068424d79f84dc8e8952896be7d9ae0a
haaeell <haikaljawahir434@gmail.com>
2024-04-27 12:29:35 +0700

    feat: integration sign up

--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -1,12 +1,13 @@
-import { useEffect } from 'react';
-import GuestLayout from '@/Layouts/GuestLayout';
+import React, { useEffect } from "react";
+import Button from "@/Components/Button";
+import Input from "@/Components/Input";
+import InputLabel from "@/Components/InputLabel";
+import { Head, Link, useForm } from "@inertiajs/react";
 import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/Button';
-import TextInput from '@/Components/TextInput';
-import { Head, Link, useForm } from '@inertiajs/react';
+import TextInput from "@/Components/TextInput";
 
 export default function Register() {
+
     const { data, setData, post, processing, errors, reset } = useForm({
         name: '',
         email: '',
@@ -21,7 +22,7 @@ export default function Register() {
     }, []);
 
     const handleOnChange = (event) => {
-        setData(event.target.name, event.target.type === 'checkbox' ? event.target.checked : event.target.value);
+        setData(event.target.name, event.target.value);
     };
 
     const submit = (e) => {
@@ -31,18 +32,39 @@ export default function Register() {
     };
 
     return (
-        <GuestLayout>
-            <Head title="Register" />
-
-            <form onSubmit={submit}>
-                <div>
+        <>
+        <Head title="Sign Up"/>
+            <div className="mx-auto max-w-screen min-h-screen bg-black text-white md:px-10 px-3">
+                <div className="fixed top-[-50px] hidden lg:block">
+                    <img
+                        src="/assets/images/signup-image.png"
+                        className="hidden laptopLg:block laptopLg:max-w-[450px] laptopXl:max-w-[640px]"
+                        alt=""
+                    />
+                </div>
+                <div className="py-24 flex laptopLg:ml-[680px] laptopXl:ml-[870px]">
+                    <div>
+                        <img src="/assets/images/moonton-white.svg" alt="" />
+                        <div className="my-[70px]">
+                            <div className="font-semibold text-[26px] mb-3">
+                                Sign Up
+                            </div>
+                            <p className="text-base text-[#767676] leading-7">
+                                Explore our new movies and get <br></br>
+                                the better insight for your life
+                            </p>
+                            <InputError errors={errors}/>
+                        </div>
+                        <form className="w-[370px]" onSubmit={submit}>
+                            <div className="flex flex-col gap-6">
+                            <div>
                     <InputLabel htmlFor="name" value="Name" />
 
                     <TextInput
                         id="name"
                         name="name"
                         value={data.name}
-                        className="mt-1 block w-full"
+                        className=" block w-full"
                         autoComplete="name"
                         isFocused={true}
                         onChange={handleOnChange}
@@ -52,15 +74,15 @@ export default function Register() {
                     <InputError message={errors.name} className="mt-2" />
                 </div>
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="email" value="Email" />
+                <div className="">
+                    <InputLabel htmlFor="email" value="Email Address" />
 
                     <TextInput
                         id="email"
                         type="email"
                         name="email"
                         value={data.email}
-                        className="mt-1 block w-full"
+                        className={`block w-full ${errors} ? 'border-red-500' : ''}`}
                         autoComplete="username"
                         onChange={handleOnChange}
                         required
@@ -69,7 +91,7 @@ export default function Register() {
                     <InputError message={errors.email} className="mt-2" />
                 </div>
 
-                <div className="mt-4">
+                <div className="">
                     <InputLabel htmlFor="password" value="Password" />
 
                     <TextInput
@@ -77,7 +99,7 @@ export default function Register() {
                         type="password"
                         name="password"
                         value={data.password}
-                        className="mt-1 block w-full"
+                        className=" block w-full"
                         autoComplete="new-password"
                         onChange={handleOnChange}
                         required
@@ -86,7 +108,7 @@ export default function Register() {
                     <InputError message={errors.password} className="mt-2" />
                 </div>
 
-                <div className="mt-4">
+                <div className="">
                     <InputLabel htmlFor="password_confirmation" value="Confirm Password" />
 
                     <TextInput
@@ -94,7 +116,7 @@ export default function Register() {
                         type="password"
                         name="password_confirmation"
                         value={data.password_confirmation}
-                        className="mt-1 block w-full"
+                        className=" block w-full"
                         autoComplete="new-password"
                         onChange={handleOnChange}
                         required
@@ -102,20 +124,29 @@ export default function Register() {
 
                     <InputError message={errors.password_confirmation} className="mt-2" />
                 </div>
-
-                <div className="flex items-center justify-end mt-4">
-                    <Link
-                        href={route('login')}
-                        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                        Already registered?
-                    </Link>
-
-                    <PrimaryButton className="ml-4" disabled={processing}>
-                        Register
-                    </PrimaryButton>
+                            </div>
+                            <div className="grid space-y-[14px] mt-[30px]">
+                                <Button type="submit" variant="primary" 
+                                        processing={processing}>
+                                    <span className="text-base font-semibold">
+                                        Sign Up
+                                    </span>
+                                </Button>
+                                <Link href={route("prototype.login")}>
+                                    <Button
+                                        type="button"
+                                        variant="light-outline"
+                                    >
+                                        <span className="text-base text-white">
+                                            Sign In to My Account
+                                        </span>
+                                    </Button>
+                                </Link>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </form>
-        </GuestLayout>
+            </div>
+        </>
     );
 }
